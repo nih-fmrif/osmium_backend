9aab376dcc4e4927de536577d4b4c9b91d7c60ec
Jan Varada <4758101+jvarada@users.noreply.github.com>
2019-02-26 16:17:36 -0500

    update authentication for siteminder

--- fmrif_base/authentication.py
+++ fmrif_base/authentication.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.db import Error
 from rest_framework import authentication
 from rest_framework import exceptions
 from fmrif_base import siteminder_headers
+from fmrif_base.utils import get_ldap_credentials
 
 User = get_user_model()
 
@@ -13,11 +15,37 @@ class FMRIFAuthentication(authentication.BaseAuthentication):
         try:
             employee_id = request.META[siteminder_headers.EMPLOYEE_ID]
         except KeyError:
-            raise exceptions.AuthenticationFailed("Siteminder error")
+            raise exceptions.AuthenticationFailed("Unable to retrieve Siteminder credentials")
 
         try:
+
             user = User.objects.get(employee_id=employee_id)
+
         except User.DoesNotExist:
-            return None
+
+            # User not in the Osmium database, check if they have credentials on the LDAP access list
+            # for Gold, and if so, create an account
+            user_data = get_ldap_credentials(employee_id=employee_id)
+
+            if user_data:
+
+                try:
+
+                    user = User.objects.create_user(
+                        employee_id=user_data['employee_id'],
+                        last_name=user_data['last_name'],
+                        first_name=user_data['first_name'],
+                        user_principal_name=user_data['user_principal_name'],
+                        username=user_data['username'],
+                        mail=user_data['mail']
+                    )
+
+                except Error:
+
+                    raise exceptions.AuthenticationFailed("Unable to create new user entry")
+
+            else:
+
+                return None
 
         return user, None
