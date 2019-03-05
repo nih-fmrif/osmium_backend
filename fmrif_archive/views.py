563cb2e5d7223902bc0893665df10ef29cd5ac91
Jan Varada <4758101+jvarada@users.noreply.github.com>
2019-03-05 15:42:43 -0500

    add filename to exam view

--- fmrif_archive/views.py
+++ fmrif_archive/views.py
@@ -117,7 +117,7 @@ class ExamView(APIView):
 
             return response
 
-        serializer = ExamSerializer(exam, context={'filename': Path(exam.filepath).name})
+        serializer = ExamSerializer(exam)
 
         return Response(serializer.data)
 
