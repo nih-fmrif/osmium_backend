d4a0428c43762e8a7592844cb1d909154b7f0369
Jan Varada <4758101+jvarada@users.noreply.github.com>
2019-03-04 21:23:44 -0500

    add user info retrieval

--- fmrif_base/serializers.py
+++ fmrif_base/serializers.py
@@ -1,4 +1,33 @@
 from rest_framework import serializers
-from . import models
-from django.contrib.auth import get_user_model
+from fmrif_base.models import FMRIFUser
 
+
+class FMRIFUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = FMRIFUser
+
+        fields = (
+            "employee_id",
+            "username",
+            "last_name",
+            "first_name",
+            "user_principal_name",
+            "mail",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+        )
+
+        read_only_fields = (
+            "employee_id",
+            "username",
+            "last_name",
+            "first_name",
+            "user_principal_name",
+            "mail",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+        )
