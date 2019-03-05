7252f555af6719b721c9e3afa479ca36ddb74a54
Jan Varada <4758101+jvarada@users.noreply.github.com>
2019-03-04 21:23:44 -0500

    add user info retrieval

--- fmrif_archive/urls.py
+++ fmrif_archive/urls.py
@@ -5,7 +5,6 @@ from fmrif_archive.views import (
     ExamView,
     DICOMInstanceList,
     # MRScanView,
-    # TestView,
 )
 
 app_name = 'fmrif_archive'
@@ -16,5 +15,4 @@ urlpatterns = [
     path('exam/<str:exam_id>/scan/<str:scan_name>/instances/', DICOMInstanceList.as_view()),
     path('exam/<str:exam_id>/revision/<int:revision>/', ExamView.as_view()),
     path('exam/<str:exam_id>/', ExamView.as_view()),
-    # path('', TestView.as_view()),
 ]
