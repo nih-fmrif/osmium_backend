d42212eaa90b3d6496573821c1b078ed73dd6da3
Jan Varada <4758101+jvarada@users.noreply.github.com>
2019-04-15 14:38:58 -0400

    update scan view url

--- fmrif_archive/urls.py
+++ fmrif_archive/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     path('exam/<str:exam_id>/revision/<int:revision>/file_collection/<str:collection_name>/',
          FileCollectionView.as_view()),
     path('exam/<str:exam_id>/file_collection/<str:collection_name>/', FileCollectionView.as_view()),
-    path('exam/<str:exam_id>/revision/<int:revision>/scan/<str:scan_name>/', MRScanView.as_view()),
-    path('exam/<str:exam_id>/scan/<str:scan_name>/', MRScanView.as_view()),
+    path('exam/<str:exam_id>/revision/<int:revision>/mr_scan/<str:scan_name>/', MRScanView.as_view()),
+    path('exam/<str:exam_id>/mr_scan/<str:scan_name>/', MRScanView.as_view()),
     path('exam/<str:exam_id>/revision/<int:revision>/', ExamView.as_view()),
     path('exam/<str:exam_id>/', ExamView.as_view()),
 ]
